fa2c253eaf245dcbd2c5a9b5656fe27ad763e278
Sangram Thapa <thapasangram64@gmail.com>
2025-10-13 21:31:53 +0545

    added

--- src/components/nav/Navbar.jsx
+++ src/components/nav/Navbar.jsx
@@ -1,5 +1,28 @@
+import { NavLink } from "react-router-dom";
+import Header from "../Header/Header";
+
 function Navbar() {
-  return <div>Navbar</div>;
+  return (
+    <>
+      <Header />
+
+      <div className="bg-white shadow-sm mt-16">
+        <div className="flex max-w-7xl mx-auto px-4 py-2">
+          <ul className="flex gap-5 text-blue-600">
+            <li>
+              <NavLink to="/">Home</NavLink>
+            </li>
+            <li>
+              <NavLink to="/about">About</NavLink>
+            </li>
+            <li>
+              <NavLink to="/contact">Contact</NavLink>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </>
+  );
 }
 
 export default Navbar;
